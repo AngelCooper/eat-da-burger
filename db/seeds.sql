08424617800eefb9ca9e93edcc96014eb6bc0ba5
AngelCooper <74269067+AngelCooper@users.noreply.github.com>
2021-03-07 16:48:57 -0600

    updated repo

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO cats (name) VALUES ('Hamburger');
+INSERT INTO cats (name) VALUES ('Cheese BUrger');
+INSERT INTO cats (name, sleepy) VALUES ('Bacon Burger', true);
+INSERT INTO cats (name, sleepy) VALUES ('Vegan Burger', true);
+INSERT INTO cats (name, sleepy) VALUES ('Kobe Burger', true);
+INSERT INTO cats (name) VALUES ('Trumpo Burger');
