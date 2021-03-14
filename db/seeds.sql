b9075edb36e7a9a5c219338032c5f8fb6eee72dc
AngelCooper <74269067+AngelCooper@users.noreply.github.com>
2021-03-13 22:14:06 -0600

    finally! it works

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-INSERT INTO cats (name) VALUES ('Hamburger');
-INSERT INTO cats (name) VALUES ('Cheese BUrger');
-INSERT INTO cats (name, sleepy) VALUES ('Bacon Burger', true);
-INSERT INTO cats (name, sleepy) VALUES ('Vegan Burger', true);
-INSERT INTO cats (name, sleepy) VALUES ('Kobe Burger', true);
-INSERT INTO cats (name) VALUES ('Trumpo Burger');
+INSERT INTO burgers (name, devour) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (name, devour) VALUES ('Hamburger', false);
+INSERT INTO burgers (name, devour) VALUES ('Bacon Burger', false);
