c8f48ce9bd2795c2b24230b93c1298b7ce0a67ff
AngelCooper <74269067+AngelCooper@users.noreply.github.com>
2021-03-14 15:59:42 -0500

    didnt work

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,14 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-let connection;
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    // NOTE: Be sure to add your MySQL password here!
-    password: 'yessir123',
-    database: 'burger_db',
-  });
-}
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  // NOTE: Be sure to add your MySQL password here!
+  password: 'yessir123',
+  database: 'burger_db',
+});
 
 // Make connection.
 connection.connect((err) => {
